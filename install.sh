5f9ff94cef77e58c09d0c317932fc7cf92d20969
張哲魁 <abc87987@gmail.com>
2019-03-17 16:59:04 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-pip3 install pillow numpy matplotlib tkinter
+pip3 install pillow numpy matplotlib
+
+# for ubuntu ONLY, if you are in different system, please install python3 tkinter manually
+sudo apt-get install python3-tkinter
